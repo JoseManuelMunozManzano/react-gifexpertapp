b05cfe651be0d3ad32e6927fbd53ca1dae6394c8
Neimerc <Neil_Mercury@yahoo.es>
2020-12-15 20:48:19 +0100

    Implementando PropTypes y Actualizar Snapshots

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 const GifGridItem = ({ title, url }) => {
   return (
     <div className="card animate__animated animate__fadeIn">
@@ -7,6 +9,11 @@ const GifGridItem = ({ title, url }) => {
       <p>{title}</p>
     </div>
   );
+};
+
+GifGridItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
 
 export default GifGridItem;
