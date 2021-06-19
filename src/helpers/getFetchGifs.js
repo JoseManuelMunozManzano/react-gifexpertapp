f21d440060c5c5315459f774ab9e0bae020d74b5
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-06-19 06:20:31 +0200

    Cambiar api key y variables de entorno

--- src/helpers/getFetchGifs.js
+++ src/helpers/getFetchGifs.js
@@ -1,11 +1,11 @@
-const getFetchGifs = async category => {
+const getFetchGifs = async (category) => {
   const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
     category
-  )}&limit=10&api_key=tYrZUEFqSi68fVUTjKj9oN5F5Ezhkqme`;
+  )}&limit=10&api_key=${process.env.REACT_APP_API_GIPHY}`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
-  const gifs = data.map(img => {
+  const gifs = data.map((img) => {
     return {
       id: img.id,
       title: img.title,
